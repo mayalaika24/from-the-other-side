dde0a2b8e4227301d792f22ba628116fc2ebbbb0
Maya Layka <mayalaika98@gmail.com>
2026-09-13 11:27:27 +0200

    reset dir name

--- utils/getData.js
+++ utils/getData.js
@@ -1,10 +1,10 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 
-export async function getData(res, baseDir) {
+export async function getData(res, dir) {
   try {
     const content = await fs.readFile(
-      path.join(baseDir, "data", "data.json"),
+      path.join(dir, "data", "data.json"),
       "utf-8",
     );
     return JSON.parse(content);
